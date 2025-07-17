da02dc2f242f7edb53ba1fb285b49c682608eb5e
Sravani Badana <mail2chinnu1437@gmail.com>
2025-07-17 15:07:42 +1000

    Updated Construction.tsx

--- src/pages/services/Construction.tsx
+++ src/pages/services/Construction.tsx
@@ -59,7 +59,7 @@ const Construction: React.FC = () => {
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {[
-              '/constructionpagebottom1.jpg',
+              '/commercialexterior.png',
               '/constructionpagebottom2.jpg',
               '/ahahoteldaytime.jpg',
               '/pujarirambabugaru.updated.png',
