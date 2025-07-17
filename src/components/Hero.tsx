439ed7d9d82f0cb3caf6340f7f50c74dde4d5db3
Sravani Badana <mail2chinnu1437@gmail.com>
2025-07-17 13:36:33 +1000

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -31,7 +31,7 @@ const Hero: React.FC = () => {
       <div 
         className="absolute inset-0 w-full h-full z-0"
         style={{
-          backgroundImage: "url('/pujarirambabugaru.updated.png')",
+          backgroundImage: "url('/herobg_blue.png')",
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat'
