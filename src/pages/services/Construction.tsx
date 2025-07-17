7ec2594a05f493319bdcddbd0445b288f89c432a
Sravani Badana <mail2chinnu1437@gmail.com>
2025-07-17 14:30:11 +1000

    Updated Construction.tsx

--- src/pages/services/Construction.tsx
+++ src/pages/services/Construction.tsx
@@ -27,7 +27,7 @@ const Construction: React.FC = () => {
         {/* Hero Image */}
         <div className="mb-12 rounded-xl overflow-hidden shadow-lg">
           <img
-            src="/renovationheroimage.jpg"
+            src="/pujarirambabugaru.updated.png"
             alt="Construction Services"
             className="w-full h-96 object-cover"
           />
