93808add4e02fb2e4ab15186f6a6bb3676f9b14b
Sravani Badana <mail2chinnu1437@gmail.com>
2025-07-19 00:52:32 +1000

    Updated Portfolio.tsx

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -99,7 +99,7 @@ const Portfolio: React.FC = () => {
             {[
               { image: '/interiorpagewoodenframe.jpg', title: 'Modern Construction', type: 'Construction' },
               { image: '/ahahotelhall.jpg', title: 'Luxury Interior', type: 'Interior Design' },
-              { image: 'https://images.pexels.com/photos/834892/pexels-photo-834892.jpeg?auto=compress&cs=tinysrgb&w=800', title: 'Office Renovation', type: 'Renovations' },
+              { image: '/meetingroominterior.jpg', title: 'Office Renovation', type: 'Renovations' },
               { image: '/meetingroominterior.jpg', title: 'Kitchen Design', type: 'Interior Design' },
               { image: 'https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg?auto=compress&cs=tinysrgb&w=800', title: 'Living Space', type: 'Interior Design' },
               { image: 'https://images.pexels.com/photos/1454804/pexels-photo-1454804.jpeg?auto=compress&cs=tinysrgb&w=800', title: 'Bedroom Design', type: 'Interior Design' },
