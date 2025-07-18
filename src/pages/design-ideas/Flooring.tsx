cbf39bf0e40fe3684fd7e65285954aa4d2a35e5d
Sravani Badana <mail2chinnu1437@gmail.com>
2025-07-18 17:48:02 +1000

    Updated Flooring.tsx

--- src/pages/design-ideas/Flooring.tsx
+++ src/pages/design-ideas/Flooring.tsx
@@ -18,7 +18,7 @@ const Flooring: React.FC = () => {
         {/* Hero Image */}
         <div className="mb-12 rounded-xl overflow-hidden shadow-lg">
           <img
-            src="/flooring_1.jpg"
+            src="/flooring_2.jpg"
             alt="Flooring Design"
             className="w-full h-96 object-cover"
           />
