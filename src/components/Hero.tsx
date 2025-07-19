541225b6ac6c7b488df20fac196534a14949be00
Sravani Badana <mail2chinnu1437@gmail.com>
2025-07-19 14:14:18 +1000

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -31,7 +31,7 @@ const Hero: React.FC = () => {
       <div 
         className="absolute inset-0 w-full h-full z-0"
         style={{
-          backgroundImage: "url('/prgroup_bluewall_hero.jpg')",
+          backgroundImage: "url('/home_hero_cow.jpg')",
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat'
